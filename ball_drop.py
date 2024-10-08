ea12604850e2ab471121d25de10598834b2fe8b7
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-10-08 11:08:56 +0900

    update & draw

--- ball_drop.py
+++ ball_drop.py
@@ -36,12 +36,28 @@ class s_Ball:
         self.frame = random.randint(0, 7)
         self.image = load_image('ball21x21.png')
 
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.y -= 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
 
 class b_Ball:
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 599
         self.frame = random.randint(0, 7)
         self.image = load_image('ball41x41.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.y -= 5
+        
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
 
 
 def handle_events():
